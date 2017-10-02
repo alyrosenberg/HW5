229dbbfbd97cb9f9bb725e31bdb85d0f45b3a9b3
Aly Rosenberg <alysonpr@umich.edu>
2017-10-02 15:43:07 -0400

    final code for hw 5 the sum was correct

--- Desktop/SI206HW5/HW5.py
+++ Desktop/SI206HW5/HW5.py
@@ -1,1 +1,16 @@
 #This is the code for my HW5
+import re
+
+readfile = open('actualdata.txt', 'r')
+content = readfile.readlines()
+
+x = list() 
+
+for line in content:
+    y = re.findall('[0-9]+',line)
+    x = x + y
+sum=0
+for numb in x:
+    sum = sum + int(numb)
+
+print (sum)
